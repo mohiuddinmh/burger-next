a8fcb033728d75eba15b7aace3948e3fbfa710c6
mohiuddinmh <mohiuddinmh@gmail.com>
2024-01-27 23:40:59 -0600

    fetch

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Cart: React.FC = () => <>Shopping Cart</>
+
+export default Cart
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Test</div>
-    </main>
-  );
+type Burger = {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
+    description: string;
+    calorie: number;
+};
+const getBurgers = async (): Promise<{burgers: Burger[]}> => {
+    const response = await fetch("https://burgerhub00.github.io/data/products.json");
+    if(!response.ok){
+        throw new Error("failed to fetch users")
+    }
+
+  const data =  await response.json()
+
+    return { burgers: data.products };
 }
+
+const Home: React.FC = async () => {
+    const {burgers} = await getBurgers()
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+            {burgers.map(burger => (
+                <div key={burger.id}>{burger.name}</div>
+            ))}
+        </main>
+    );
+}
+export default Home
