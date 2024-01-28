cea1a70d008ea8e1df6abb72dbb117010e7681e0
mohiuddinmh <mohiuddinmh@gmail.com>
2024-01-28 15:10:48 -0600

    implemented BurgerCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
+import {Burger} from "@/types";
+import BurgerCard from "@/components/BurgerCard/BurgerCard";
 
-type Burger = {
-    id: string;
-    name: string;
-    price: number;
-    image: string;
-    description: string;
-    calorie: number;
-};
 const getBurgers = async (): Promise<{burgers: Burger[]}> => {
     const response = await fetch("https://burgerhub00.github.io/data/products.json");
     if(!response.ok){
@@ -22,11 +16,12 @@ const getBurgers = async (): Promise<{burgers: Burger[]}> => {
 const Home: React.FC = async () => {
     const {burgers} = await getBurgers()
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-            {burgers.map(burger => (
-                <div key={burger.id}>{burger.name}</div>
-            ))}
+        <main className="flex flex-col items-center justify-center p-24">
+            <div className="grid grid-cols-4 gap-4">
+                {burgers.map(burger => (
+                    <BurgerCard key={burger.id} burger={burger} />
+                ))}
+            </div>
         </main>
     );
 }
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Burger = {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
+    description: string;
+    calorie: number;
+};
