05327d57ae2aad55d0982d4616dc60b24795a556
mohiuddinmh <mohiuddinmh@gmail.com>
2024-02-01 14:08:51 -0600

    final touches

--- src/app/[burgerId]/page.tsx
+++ src/app/[burgerId]/page.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import {getBurgers} from "@/services/burgerService";
 import AddToCart from "@/components/AddToCart/AddToCart";
+import Link from "next/link";
 
 const BurgerDetail: React.FC<{ params: { burgerId: string } }> = async ({params: {burgerId}}) => {
     const {burgers} = await getBurgers()
 
     const burger = burgers.find(b => b.id === burgerId)
-    return <div className="flex justify-center items-center h-screen">
+    return <div className="flex justify-center items-center">
         {burger && (
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img className="w-full" src={burger.image} alt={burger.name}/>
+                <div className="p-4">
+                    <Link href="/"
+                          passHref
+                          className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out font-semibold">
+                        &#8592; Go Back
+                    </Link>
+                </div>
+                <div>
+                    <img src={burger.image} alt={burger.name} className="w-full h-64 object-cover"/>
+                </div>
+
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{burger.name}</div>
                     <p className="text-gray-700 text-base">
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,39 +6,53 @@ import {useOrderContext} from "@/context/OrderContext";
 const Cart: React.FC = () => {
     const {cartItems, removeCartItem} = useOrderContext()
 
+    const handlePlaceOrder = () => {
+    }
+    
     return (
         <div className="max-w-4xl mx-auto mt-10 p-4 shadow-lg rounded-lg">
             <h2 className="text-3xl font-bold mb-6">Shopping Cart</h2>
             {cartItems.length > 0 ? (
-                <div>
-                    {cartItems.map((item, index) => (
-                        <div key={index} className="flex items-center justify-between border-b-2 py-4">
-                            <div className="flex items-center">
-                                <img src={item.burger.image} alt={item.burger.name}
-                                     className="h-16 w-16 object-cover rounded mr-4"/>
-                                <div>
-                                    <h3 className="text-lg font-semibold">{item.burger.name}</h3>
-                                    <p className="text-gray-600">${item.burger.price / 100} each</p>
+                <>
+                    <div>
+                        {cartItems.map((item, index) => (
+                            <div key={index} className="flex items-center justify-between border-b-2 py-4">
+                                <div className="flex items-center">
+                                    <img src={item.burger.image} alt={item.burger.name}
+                                         className="h-16 w-16 object-cover rounded mr-4"/>
+                                    <div>
+                                        <h3 className="text-lg font-semibold">{item.burger.name}</h3>
+                                        <p className="text-gray-600">${item.burger.price / 100} each</p>
+                                    </div>
                                 </div>
+                                <div className="flex items-center">
+                                    <span className="text-sm font-medium mr-4">Qty: {item.quantity}</span>
+                                    <span className="font-bold">${(item.burger.price / 100) * item.quantity}</span>
+                                </div>
+                                <button
+                                    className="ml-4 px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300"
+                                    onClick={() => removeCartItem(item.burger.id)}
+                                >
+                                    Remove
+                                </button>
                             </div>
-                            <div className="flex items-center">
-                                <span className="text-sm font-medium mr-4">Qty: {item.quantity}</span>
-                                <span className="font-bold">${(item.burger.price / 100) * item.quantity}</span>
-                            </div>
-                            <button
-                                className="ml-4 px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300"
-                                onClick={() => removeCartItem(item.burger.id)}
-                            >
-                                Remove
-                            </button>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                    <div className="mt-6 flex justify-center">
+                        <button
+                            onClick={handlePlaceOrder}
+                            className="px-6 py-2 bg-yellow-500 text-white font-bold rounded hover:bg-yellow-600 transition duration-300"
+                        >
+                            Place Order
+                        </button>
+                    </div>
+                </>
             ) : (
                 <div className="text-center py-6">Your cart is empty.</div>
             )}
         </div>
     )
+
 }
 
 export default Cart
